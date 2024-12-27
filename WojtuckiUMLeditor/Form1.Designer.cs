0e18f5ec603efd4322bb82baeaea8224927a25d9
Dominik Wojtucki <dominikwojtucki8@gmail.com>
2024-12-27 19:22:07 +0100

    mazání tříd

--- WojtuckiUMLeditor/Form1.Designer.cs
+++ WojtuckiUMLeditor/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             pictureBoxCanvas = new PictureBox();
             buttonAddClass = new Button();
+            buttonEditClass = new Button();
+            buttonDeleteClass = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBoxCanvas).BeginInit();
             SuspendLayout();
             // 
@@ -40,7 +42,7 @@
             pictureBoxCanvas.Name = "pictureBoxCanvas";
             pictureBoxCanvas.Size = new Size(800, 450);
             pictureBoxCanvas.TabIndex = 0;
-            pictureBoxCanvas.TabStop = false;
+            pictureBoxCanvas.TabStop = false;            
             pictureBoxCanvas.Paint += pictureBoxCanvas_Paint;
             pictureBoxCanvas.MouseClick += pictureBoxCanvas_MouseClick;
             pictureBoxCanvas.MouseDown += pictureBoxCanvas_MouseDown;
@@ -49,19 +51,40 @@
             // 
             // buttonAddClass
             // 
-            buttonAddClass.Location = new Point(646, 0);
+            buttonAddClass.Location = new Point(649, 12);
             buttonAddClass.Name = "buttonAddClass";
-            buttonAddClass.Size = new Size(154, 33);
+            buttonAddClass.Size = new Size(150, 50);
             buttonAddClass.TabIndex = 1;
             buttonAddClass.Text = "ADD CLASS";
             buttonAddClass.UseVisualStyleBackColor = true;
             buttonAddClass.Click += buttonAddClass_Click;
+            // 
+            // buttonEditClass
+            // 
+            buttonEditClass.Location = new Point(650, 68);
+            buttonEditClass.Name = "buttonEditClass";
+            buttonEditClass.Size = new Size(150, 50);
+            buttonEditClass.TabIndex = 2;
+            buttonEditClass.Text = "EDIT CLASS";
+            buttonEditClass.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteClass
+            // 
+            buttonDeleteClass.Location = new Point(650, 124);
+            buttonDeleteClass.Name = "buttonDeleteClass";
+            buttonDeleteClass.Size = new Size(150, 50);
+            buttonDeleteClass.TabIndex = 3;
+            buttonDeleteClass.Text = "DELETE CLASS";
+            buttonDeleteClass.UseVisualStyleBackColor = true;
+            buttonDeleteClass.Click += buttonDeleteClass_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(buttonDeleteClass);
+            Controls.Add(buttonEditClass);
             Controls.Add(buttonAddClass);
             Controls.Add(pictureBoxCanvas);
             Name = "Form1";
@@ -74,5 +97,7 @@
 
         private PictureBox pictureBoxCanvas;
         private Button buttonAddClass;
+        private Button buttonEditClass;
+        private Button buttonDeleteClass;
     }
 }
--- WojtuckiUMLeditor/Form1.cs
+++ WojtuckiUMLeditor/Form1.cs
@@ -12,6 +12,7 @@
 
         private Rectangle? selectedClass = null;
         private Point lastMousePosition;
+        private bool isDragging = false;
 
 
         public Form1()
@@ -29,8 +30,20 @@
                 g.FillRectangle(Brushes.LightBlue, rect);
                 g.DrawRectangle(Pens.Black, rect);
 
-                g.DrawString("ClassName", new Font("Arial", 12), Brushes.Black, rect.X + 5, rect.Y + 5);
+                StringFormat stringFormat = new StringFormat
+                {
+                    Alignment = StringAlignment.Center, 
+                    LineAlignment = StringAlignment.Center 
+                };
+
+                g.DrawString("ClassName", new Font("Arial", 12), Brushes.Black, rect, stringFormat);
+
+                if (selectedClass != null)
+                {
+                    g.DrawRectangle(Pens.Red, selectedClass.Value);
+                }
             }
+            
         }
 
         private void buttonAddClass_Click(object sender, EventArgs e)
@@ -47,30 +60,54 @@
 
         private void pictureBoxCanvas_MouseClick(object sender, MouseEventArgs e)
         {
+            bool clickedOnClass = false;
+            
+            foreach (var rect in Classes)
+            {
+                if (rect.Contains(e.Location)) 
+                {
+                    clickedOnClass = true;
+                    selectedClass = rect; 
+                    break;
+                }
+            }
+
+            
+            if (!clickedOnClass)
+            {
+                selectedClass = null;
+            }
+
+            pictureBoxCanvas.Invalidate(); 
+
 
         }
 
         private void pictureBoxCanvas_MouseDown(object sender, MouseEventArgs e)
-        {
+        {            
+
             foreach (var rect in Classes)
             {
                 if (rect.Contains(e.Location))
                 {
                     selectedClass = rect;
                     lastMousePosition = e.Location;
+                    isDragging = true;
                     break;
                 }
             }
+
+            pictureBoxCanvas.Invalidate();
         }
 
         private void pictureBoxCanvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if (selectedClass != null && e.Button == MouseButtons.Left) 
+            if (selectedClass != null && e.Button == MouseButtons.Left)
             {
                 int dx = e.X - lastMousePosition.X;
-                int dy = e.Y - lastMousePosition.Y; 
+                int dy = e.Y - lastMousePosition.Y;
 
-                
+
                 var updatedRect = new Rectangle(
                     selectedClass.Value.X + dx,
                     selectedClass.Value.Y + dy,
@@ -78,18 +115,36 @@
                     selectedClass.Value.Height
                 );
 
-                Classes.Remove(selectedClass.Value); 
-                Classes.Add(updatedRect);           
-                selectedClass = updatedRect;        
+                Classes.Remove(selectedClass.Value);
+                Classes.Add(updatedRect);
+                selectedClass = updatedRect;
 
-                lastMousePosition = e.Location;     
-                pictureBoxCanvas.Invalidate();      
+                lastMousePosition = e.Location;
+                pictureBoxCanvas.Invalidate();
             }
         }
 
         private void pictureBoxCanvas_MouseUp(object sender, MouseEventArgs e)
         {
-            selectedClass = null;
+            isDragging = false;
         }
+
+        private void buttonDeleteClass_Click(object sender, EventArgs e)
+        {
+            if (selectedClass != null)
+            {
+                Classes.Remove(selectedClass.Value);
+                selectedClass = null;
+                pictureBoxCanvas.Invalidate();
+
+            }
+            else
+            {
+                MessageBox.Show("Vyberte třídu kliknutím na ni.", "Upozornění", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+        
     }
 }
